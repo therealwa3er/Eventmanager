29d3001d86fa8c47868548643c460fde7a1bf821
therealwa3er <31991819+therealwa3er@users.noreply.github.com>
2018-04-10 09:17:12 +0200

    Relation beetween Events and Subscribers

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,3 @@
 class Event < ApplicationRecord
+  has_many :subscribers, :foreign_key => "event_id", :class_name => "Event"  
 end
--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,3 +1,4 @@
 class Subscriber < ApplicationRecord
-  belongs_to :Event
+  belongs_to :Event, :foreign_key => "event_id", :class_name => "Subscriber"
+
 end
