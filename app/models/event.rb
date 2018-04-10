a5a02b045df9acb2097f82668a3e52b156b72ebb
therealwa3er <31991819+therealwa3er@users.noreply.github.com>
2018-04-10 09:54:56 +0200

    Validation working.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
-  has_many :subscribers, :foreign_key => "event_id", :class_name => "Event"  
+  has_many :subscribers, :foreign_key => "event_id", :class_name => "Event"
+
+  validates :title, presence: {
+    message: "le nom doit être renseigné."
+  }
 end
--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,4 +1,7 @@
 class Subscriber < ApplicationRecord
   belongs_to :Event, :foreign_key => "event_id", :class_name => "Subscriber"
 
+  validates :name, presence: {
+    message: "le nom doit être renseigné."
+  }
 end
