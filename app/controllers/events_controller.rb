ae1b72fef06163a90117e599395a2134b4f08e4a
therealwa3er <31991819+therealwa3er@users.noreply.github.com>
2018-05-03 16:32:17 +0200

    changing the flash and the label

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -28,7 +28,8 @@ class EventsController < ApplicationController
 
     respond_to do |format|
       if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        flash[:success]= 'Event was successfully created.'
+        format.html { redirect_to @event }
         format.json { render :show, status: :created, location: @event }
       else
         format.html { render :new }
@@ -42,7 +43,8 @@ class EventsController < ApplicationController
   def update
     respond_to do |format|
       if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+        flash[:success]= 'Event was successfully updated.'
+        format.html { redirect_to @event }
         format.json { render :show, status: :ok, location: @event }
       else
         format.html { render :edit }
@@ -56,7 +58,8 @@ class EventsController < ApplicationController
   def destroy
     @event.destroy
     respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+      flash[:success]= 'Event was successfully destroyed.'
+      format.html { redirect_to events_url }
       format.json { head :no_content }
     end
   end
